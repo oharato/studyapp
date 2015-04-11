56b44318b088c044649c0b125076339b18947528
OHARATO <oharato@live.jp>
2015-04-11 09:56:50 +0000

    devise twitter

--- studyapp/config/routes.rb
+++ studyapp/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'quizzes#index'
-  resources :quizzes
+  get 'home/index'
 
+  root 'home#index'
+  resources :quizzes
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- studyapp/db/schema.rb
+++ studyapp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402140018) do
+ActiveRecord::Schema.define(version: 20150411090602) do
 
   create_table "quizzes", force: true do |t|
     t.text     "question",   null: false
@@ -21,5 +21,14 @@ ActiveRecord::Schema.define(version: 20150402140018) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
 
 end
