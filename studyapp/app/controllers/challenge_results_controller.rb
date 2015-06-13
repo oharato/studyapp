1fc44634f379d9fdb3bf848439c79f2e2be2bb2b
OHARATO <oharato@live.jp>
2015-06-13 08:39:24 +0000

    challenge fixed

--- studyapp/app/controllers/challenge_results_controller.rb
+++ studyapp/app/controllers/challenge_results_controller.rb
@@ -17,8 +17,16 @@ class ChallengeResultsController < ApplicationController
   end
   
   def next
-    target_quizz_ids = @quizzes.ids
-    next_quiz = Quiz.find target_quizz_ids.sample
+    target_quiz_ids = @quizzes.ids
+    current_quiz_index = target_quiz_ids.index(params[:id].try(:to_i))
+    next_quiz_id =  
+      if current_quiz_index.nil? || current_quiz_index+1 == target_quiz_ids.size
+        target_quiz_ids.first
+      else
+        target_quiz_ids[current_quiz_index+1]
+      end
+
+    next_quiz = Quiz.find next_quiz_id #target_quiz_ids.sample
     redirect_to challenge_results_challenge_path(id: next_quiz.id)
   end
 
@@ -36,7 +44,14 @@ class ChallengeResultsController < ApplicationController
 
   private
   
-　def set_target_quizzes
+  
+  def create(correct)
+    @quiz = Quiz.find(params[:id])
+    current_user.challenge_results.create(quiz: @quiz, correct: correct)
+    redirect_to challenge_results_next_path(id: @quiz.id)
+  end
+
+  def set_target_quizzes
     @keyword = session[:keyword] 
     @tags = session[:tags]
     tag_array = @tags.try(:split, ',')
@@ -44,13 +59,7 @@ class ChallengeResultsController < ApplicationController
     @quizzes = Quiz.search(@keyword)
       .tap{|x| break x.tagged_with(tag_array) if tag_array.present? }
       .tap{|x| break x.joins(:stars).where(stars: {user_id: current_user.id}) if @stared }
-　  
-　end
-  
-  def create(correct)
-    @quiz = Quiz.find(params[:id])
-    current_user.challenge_results.create(quiz: @quiz, correct: correct)
-    redirect_to challenge_results_next_path
+      .order('quizzes.id')
   end
 
 end
