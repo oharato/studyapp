1a324fa38d2f1fbe9ab70734d720c58db0093ba0
oharato <oharato@live.jp>
2018-04-08 21:17:26 +0900

    render text -> plain

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -2,16 +2,16 @@ class StarsController < ApplicationController
 
   def create
     Quiz.find(params[:quiz_id]).stars.create!(user: current_user)
-    render text: "create"
+    render plain: "create"
   rescue => e
-    render text: e
+    render plain: e
   end
  
   def destroy
     Quiz.find(params[:id]).stars.where(user: current_user).delete_all
-    render text: "destroy"
+    render plain: "destroy"
   rescue => e
-    render text: e
+    render plain: e
   end
 
 end
